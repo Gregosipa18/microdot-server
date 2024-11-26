d8a301598ac33702e7aeff675c7b78a9ad831348
Gregosipa18 <gregorysipa@gmail.com>
2024-11-26 12:58:42 -0300

    Feat: agrego cambios de base.css y el boot.py

--- actividades/ej_01/boot.py
+++ actividades/ej_01/boot.py
@@ -1,1 +1,14 @@
-# Configuracion inicial
+# Configuración de red para conexión inicial
+def initialize_connection():
+    import network
+    wifi_interface = network.WLAN(network.STA_IF)
+
+    if not wifi_interface.isconnected():
+        print('Intentando conectar a la red...')
+        wifi_interface.active(True)
+        wifi_interface.connect('Cooperadora Alumnos', '')
+        
+        while not wifi_interface.isconnected():
+            continue  # Espera activa hasta conectarse
+
+    print('Configuración de red establecida:', wifi_interface.ifconfig())
