ea5d3ab9852a94a7975d19006c155c2668ef9f0f
Gregosipa18 <gregorysipa@gmail.com>
2024-11-26 11:49:13 -0300

    Feat: agrregar app.py de programa que muestra texto centrado que contenga algún mensaje personalizado

--- actividades/ej_01/app.py
+++ actividades/ej_01/app.py
@@ -1,1 +1,22 @@
 # Aplicacion del servidor
+# Servidor básico para entregar archivos
+from boot import initialize_connection
+from microdot import Microdot, send_file
+
+# Conexión inicial
+initialize_connection()
+
+# Configuración de la aplicación
+server = Microdot()
+
+@server.route('/')
+async def home(request):
+    return send_file('index.html')
+
+@server.route('/<folder>/<filename>')
+async def serve_files(request, folder, filename):
+    file_path = f"{folder}/{filename}"
+    return send_file(file_path)
+
+# Ejecutar servidor en el puerto 80
+server.run(port=80)
